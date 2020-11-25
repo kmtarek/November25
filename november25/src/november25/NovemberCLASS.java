ecf9d33f835611956f80d781ff4bea1512422ca3
kmtarek <kmtarekfca@gmail.com>
2020-11-25 18:52:18 -0500

    nov commit

---
+++ november25/src/november25/NovemberCLASS.java
@@ -0,0 +1,14 @@
+package november25;
+
+public class NovemberCLASS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        int a = 10;
+		int b = 23;
+	int c = (a+b);
+		System.out.println(c);
+	}
+
+}
